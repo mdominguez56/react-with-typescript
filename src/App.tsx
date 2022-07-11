269370b7e7fc51eb6db56ba6c8cf14d5d24eb947
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-07-11 07:56:09 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const INITIAL_STATE = [
+  {
+    nick: "Subscriptor 1",
+    subMonths: 1,
+    avatar: "https://i.pravatar.cc/150?u=sub1",
+    description: "Es un sub normal",
+  },
+  {
+    nick: "Subscriptor 2",
+    subMonths: 3,
+    avatar: "https://i.pravatar.cc/150?u=sub2",
+  },
+];
+
+interface Sub {
+  nick: string;
+  avatar: string;
+  subMonths: number;
+  description?: string;
+}
 
 function App() {
+  const [subs, setSubs] = useState<Array<Sub>>();
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mati subs</h1>
+      <ul>
+        {subs?.map((sub) => {
+          return (
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+              <h4>
+                {sub.nick} (<small>{`${sub.subMonths} months`}</small>)
+              </h4>
+              <p>{sub.description}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
